b4077de7b0b35b067f747d3391cd9f132b07db51
ThinkAboutRek <SB2018@canterbury.ac.uk>
2024-10-23 10:53:43 +0100

    knex.destroy() added to the test files so the warnings are gone and knex is properly closed(also the server.close())

--- tests/sets.test.js
+++ tests/sets.test.js
@@ -1,15 +1,30 @@
 const request = require('supertest');
 const app = require('../index');
+const knex = require('../db/knex');
+
+let server;
+
+beforeAll(async () => {
+  server = app.listen(4000, () => {
+    console.log('Test server running on http://localhost:4000');
+  });
+  await knex.migrate.latest(); 
+});
+
+afterAll(async () => {
+  await server.close(); 
+  await knex.destroy(); 
+});
 
 describe('Flashcard Set Endpoints', () => {
   it('should fetch all flashcard sets', async () => {
-    const res = await request(app).get('/api/sets');
+    const res = await request(server).get('/api/sets');
     expect(res.statusCode).toEqual(200);
     expect(Array.isArray(res.body)).toBeTruthy();
   });
 
   it('should create a new flashcard set', async () => {
-    const res = await request(app)
+    const res = await request(server)
       .post('/api/sets')
       .send({
         name: 'Test Set',
